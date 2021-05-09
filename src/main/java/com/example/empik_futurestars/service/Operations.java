446e02a7e7a5c339baef58bef01b1fdc35faa19d
Lukasz Tolpa <s15525@pjwstk.edu.pl>
2021-05-09 22:31:34 +0200

    Step 3

--- src/main/java/com/example/empik_futurestars/service/Operations.java
+++ src/main/java/com/example/empik_futurestars/service/Operations.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.regex.Pattern;
+
 @Service
 public class Operations {
     private int result;
@@ -11,16 +13,24 @@ public class Operations {
     public Operations() {
     }
 
-    public int Add(String numbers){
+    public int Add(String numbers) {
+        Pattern pattern = Pattern.compile(",\\\\");
 
-        String[] splitNumbers = numbers.split(",");
+        if (pattern.matcher(numbers).find()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad char before new line expresion!");
+        } else {
+            String[] splitNumbers = numbers.split("\\\\n|,");
 
-        for (String number:
-             splitNumbers) {
-            result += Integer.parseInt(number);
+            for (String number :
+                    splitNumbers) {
+                try {
+                    result += Integer.parseInt(number);
+                }catch(NumberFormatException ignored){
+
+                }
+            }
+            return result;
         }
-
-        return result;
     }
 
     public void setResult(int result) {
